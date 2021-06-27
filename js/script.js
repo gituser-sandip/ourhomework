b059ad606a9314793baf1555d06211c69434d53f
gituser-sandip <83334244+gituser-sandip@users.noreply.github.com>
2021-06-27 01:09:15 -0700

    ourhomework website

---
+++ js/script.js
@@ -0,0 +1,134 @@
+var computer = document.getElementById("computer-section");
+var c_1 = document.getElementById("c-1");
+var c_0 = document.getElementById("c-0");
+
+var science = document.getElementById("science-section");
+var s_1 = document.getElementById("s-1");
+var s_0 = document.getElementById("s-0");
+
+var math =  document.getElementById("math-section");
+var m_1 = document.getElementById("m-1");
+var m_0 = document.getElementById("m-0");
+
+var opt =  document.getElementById("opt-section");
+var o_1 = document.getElementById("o-1");
+var o_0 = document.getElementById("o-0");
+
+var social =  document.getElementById("social-section");
+var so_1 = document.getElementById("so-1");
+var so_0 = document.getElementById("so-0");
+
+var english =  document.getElementById("english-section");
+var e_1 = document.getElementById("e-1");
+var e_0 = document.getElementById("e-0");
+
+var nepali =  document.getElementById("nepali-section");
+var n_1 = document.getElementById("n-1");
+var n_0 = document.getElementById("n-0");
+
+var health =  document.getElementById("health-section");
+var h_1 = document.getElementById("h-1");
+var h_0 = document.getElementById("h-0");
+
+var account =  document.getElementById("account-section");
+var a_1 = document.getElementById("a-1");
+var a_0 = document.getElementById("a-0");
+ 
+var ovte =  document.getElementById("occupation-section");
+var oc_1 = document.getElementById("oc-1");
+var oc_0 = document.getElementById("oc-0");
+
+var moral =  document.getElementById("moral-section");
+var mo_1 = document.getElementById("mo-1");
+var mo_0 = document.getElementById("mo-0");
+
+var gk =  document.getElementById("gk-section");
+var gk_1 = document.getElementById("g-1");
+var gk_0 = document.getElementById("g-0");
+
+c_1.onclick = function(){
+	computer.style.display = "block";
+};
+c_0.onclick = function(){
+	computer.style.display = "none";
+};
+
+s_1.onclick = function(){
+	science.style.display = "block";
+};
+s_0.onclick = function(){
+	science.style.display = "none";
+};
+
+m_1.onclick = function(){
+	math.style.display = "block";
+};
+m_0.onclick = function(){
+	math.style.display = "none";
+};
+
+o_1.onclick = function(){
+	opt.style.display = "block";
+};
+o_0.onclick = function(){
+	opt.style.display = "none";
+};
+
+so_1.onclick = function(){
+	social.style.display = "block";
+};
+so_0.onclick = function(){
+	social.style.display = "none";
+};
+
+e_1.onclick = function(){
+	english.style.display = "block";
+};
+e_0.onclick = function(){
+	english.style.display = "none";
+};
+
+n_1.onclick = function(){
+	nepali.style.display = "block";
+};
+n_0.onclick = function(){
+	nepali.style.display = "none";
+};
+
+h_1.onclick = function(){
+	health.style.display = "block";
+};
+h_0.onclick = function(){
+	health.style.display = "none";
+};
+
+
+a_1.onclick = function(){
+	account.style.display = "block";
+};
+a_0.onclick = function(){
+	account.style.display = "none";
+};
+
+oc_1.onclick = function(){
+	ovte.style.display = "block";
+};
+oc_0.onclick = function(){
+	ovte.style.display = "none";
+};
+
+
+mo_1.onclick = function(){
+	moral.style.display = "block";
+};
+mo_0.onclick = function(){
+	moral.style.display = "none";
+};
+
+gk_1.onclick = function(){
+	gk.style.display = "block";
+};
+gk_0.onclick = function(){
+	gk.style.display = "none";
+};
+
